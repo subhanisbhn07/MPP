3d4423ef74b117ca5d10982712b684322f302c3f
S S <subhanisbhn07@gmail.com>
2025-09-01 14:41:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: 'compare-phones',
-  appId: '1:606285287595:web:7f6b8afd33d2012e0867ee',
-  storageBucket: 'compare-phones.firebasestorage.app',
-  apiKey: 'AIzaSyA6FuJFmgmle35tu6QlCQUTitLvnrw837s',
-  authDomain: 'compare-phones-8ae19.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '606285287595',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
