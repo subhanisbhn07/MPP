76f847eb3c3c1605277a553a0ba42f97bbdfec6d
S S <subhanisbhn07@gmail.com>
2025-09-19 05:24:15 +0000

    reduce the spacing between hero banner and top bar menu from -6 to -2 (_

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
             ) : (
               <div className="flex flex-col min-h-screen">
                   <Header />
-                  <main className="flex-1 pt-4">{children}</main>
+                  <main className="flex-1">{children}</main>
                   <Footer />
               </div>
             )}
